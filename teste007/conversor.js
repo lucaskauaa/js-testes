3f8d3f51f47f401ce26588ee2319f260e3853310
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2022-06-29 22:23:49 -0300

    criei teste 007

---
+++ teste007/conversor.js
@@ -0,0 +1,9 @@
+var texto = document.getElementById('texto')
+
+function maiusc() {
+    texto.value = texto.value.toUpperCase()
+}
+
+function minusc() {
+    texto.value = texto.value.toLowerCase()
+}
