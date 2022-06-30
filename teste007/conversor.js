9084e5381b387f491e28e7533cb056df14a1d603
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2022-06-30 09:28:24 -0300

    movi teste 007

--- teste007/conversor.js
+++
@@ -1,9 +0,0 @@
-var texto = document.getElementById('texto')
-
-function maiusc() {
-    texto.value = texto.value.toUpperCase()
-}
-
-function minusc() {
-    texto.value = texto.value.toLowerCase()
-}
