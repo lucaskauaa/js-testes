8959e635295852e17b0255c5b90bff20b6366946
Lucas Kauã <104635659+lucaskauaa@users.noreply.github.com>
2022-07-05 11:03:31 -0300

    alterei teste 006

--- teste006/script.js
+++ teste006/script.js
@@ -1,68 +1,47 @@
-var num = (document.getElementById('num'))
-var slct = document.getElementById('slct')
+var valores = document.getElementById('valores')
+var select = document.getElementById('select')
 var res = document.getElementById('res')
-var valores = []
-
-function isNumero(n) {
-    if  (Number(n) >= 1 && Number(n) <= 100){
-        return true
-    } else {
-        return false
-    }
-}
-
-function inLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true 
-    } else {
-        return false
-    }
-}
+var val = []
 
 function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} foi adicionado.`
-        slct.appendChild(item)
-        res.innerHTML = ''
-
-    }   else {
-        window.alert ('Valor inválido ou já encontrado na lista')
+    res.innerHTML = ''
+    if (valores.value < 1 || valores.value > 100 || valores.value.length == 0) {
+        window.alert('Insira um valor válido')
+    } else if (val.indexOf(Number(valores.value)) != -1) {
+        window.alert('Valor já digitado')
+    } else {
+        val.push(Number(valores.value))
+        var option = document.createElement('option')
+        option.setAttribute('value',val)
+        option.text = `Valor ${valores.value} adicionado.`
+        select.appendChild(option)
+        valores.value = ''
+        valores.focus()
     }
-    num.value = ''
-    num.focus()
 }
 
 function finalizar() {
-    if (valores.length == 0) {
-        window.alert ('Valor inválido ou já encontrado na lista')
+    if (val.length == 0) {
+        window.alert('Insira um valor')
     } else {
-        let total = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = 0
-        let media = 0
-        for (let pos in valores) {
-            soma += valores[pos]
-            if (valores[pos] > maior) {
-                maior = valores[pos]
+        res.innerHTML = `<p>Ao todo temos ${val.length} números cadastrados.</p>`
+        var maior = val[0]
+        var menor = val[0]
+        var soma = 0
+        var media = 0
+        for (var c in val) {
+            if (val[c] > maior) {
+               maior = val[c]
             }
-            if (valores[pos] < menor) {
-                menor = valores[pos]
+            if (val[c] < menor) {
+                menor = val[c]
             }
+            soma += Number(val[c])
+            media = soma / val.length
         }
-        media = soma / total
-        res.innerHTML = ''
-        res.innerHTML += `<p><p>Ao todo temos ${total} números cadastrados</p>`
-        res.innerHTML += `<p>O menor valor foi ${menor}</p>`
-        res.innerHTML += `<p>O maior valor foi ${maior}</p>`
-        res.innerHTML += `<p>A soma dos valores ${soma}</p>`
-        res.innerHTML += `<p>A média dos valores ${media}</p>`
+        res.innerHTML += `<p>Maior valor encontrado foi ${maior}</p>`
+        res.innerHTML += `<p>Menor valor encontrado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores é ${media.toFixed(2)}</p>`
     }
 }
-
-/*for (var c = 0;c <= total; c++) {
-    var soma = soma + valores[c]
-}
-window.alert (Number(soma))*/
